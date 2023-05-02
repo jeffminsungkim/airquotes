991da071f8f7867af1fa1a11697698ea28c26859
jeffminsungkim <jeffminsungkim@gmail.com>
2023-05-03 07:49:11 +0900

    added @vercel/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppType } from "next/app";
 import "@/styles/globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "react-hot-toast";
 import { api } from "@/utils/api";
 
@@ -8,6 +9,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
       <Toaster position="bottom-center" toastOptions={{ duration: 2000 }} />
     </>
   );
